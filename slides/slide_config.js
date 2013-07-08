34cb791760949bfc01985e3cbf6be6b26a96b8d6
Dmytro Lypai <dmytrolypai@gmail.com>
2013-07-08 18:11:41 +0300

    Add title and 'thank you' slides.

---
+++ slides/slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Live Intro to Haskell',
+    eventInfo: {
+      title: 'Script\'n\'Code',
+      date: '18.07.2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Dmytro Lypai',
+    company: 'Chalmers University of Technology',
+    www: 'dmytrolypai'
+  }]
+};
+
